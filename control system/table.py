42a06c35f47fcc3fce9b73d3e67dde98db2366d9
bshrram <bshoor96@gmail.com>
2020-06-22 02:58:53 +0300

    create Table class and base methods

---
+++ control system/table.py
@@ -0,0 +1,23 @@
+from cell import Cell
+
+class Table:
+    """Table class that contains cells
+    Attributes:
+        None
+    """
+    def __init__(self, cells):
+        """Initialize variables used by Table class
+        Args:
+            cells: list of cells objects
+        """
+        self.cells = []
+        for i in range(len(cells)):
+            self.cells.append(Cell(cells[i]))
+
+    
+    def addCell(self, cell):
+        """Add cell to tabel.cells
+            Args:
+                cell: cell object
+        """
+        self.append(Cell(cell))
