3b07e3857c0f17e2b122d693c429c6b3357e4f84
bshrram <bshoor96@gmail.com>
2020-06-22 05:55:07 +0300

    move functions to common.py

--- control system/cell.py
+++ control system/cell.py
@@ -2,21 +2,7 @@ import math
 import pymata_express
 import numpy
 from motor import Motor
-
-def translate(value, leftMin, leftMax, rightMin, rightMax):
-    # Figure out how 'wide' each range is
-    leftSpan = leftMax - leftMin
-    rightSpan = rightMax - rightMin
-
-    # Convert the left range into a 0-1 range (float)
-    valueScaled = float(value - leftMin) / float(leftSpan)
-
-    # Convert the 0-1 range into a value in the right range.
-    return rightMin + (valueScaled * rightSpan)
-
-
-def constrain(val, min_val, max_val):
-    return min(max_val, max(min_val, val))
+from common import *
 
 class Cell:
     """Cell class that represents a cell in table
