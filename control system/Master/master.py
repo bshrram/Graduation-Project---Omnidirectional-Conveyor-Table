f5396a9ad60bf5cff2974e4a746e5c384b836f34
bshrram <bshoor96@gmail.com>
2020-06-22 20:57:44 +0300

    handle motor

--- control system/Master/master.py
+++ control system/Master/master.py
@@ -1,5 +1,20 @@
-def digWrite (pin, value):
+from pymata4 import pymata4
+
+def init_pins():
     pass
 
-def digI2cWrite(pin, value):
-    pass
+def handleMotor(pins, code):  # **** TODO
+    """
+    handle Motor to control. \n
+    Args: \n
+        pins: dict
+        code: int: 
+            1: digital in slave, pwm in master
+            2: digital in master, pwm in slave
+            3: digital & pwm in slave
+            4: digital & pwm in master
+    """
+    
+
+
+board = pymata4.Pymata4()
