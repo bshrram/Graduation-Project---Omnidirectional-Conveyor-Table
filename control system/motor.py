b44dd919dab7dfd87bf7f5495f8060a3322e9ee0
bshrram <bshoor96@gmail.com>
2020-06-22 05:55:35 +0300

    Update motor.py

--- control system/motor.py
+++ control system/motor.py
@@ -1,3 +1,28 @@
+from Master.master import *
+
 class Motor:
-    def __init__(self):
-        pass
+    """Motor class that represents motors
+    Attributes:
+        None
+    """
+
+    def __init__(self, motor):
+        """Initialize variables used by Motor class
+        Args:
+            motor: dict represents motor data:
+                motor.id: int
+                motor.pins: dict:
+                    {'digital':tuple, 'pwm': int, 'digMaster': boolean, 'pwmMaster': boolean}
+        """
+        self.id = motor.id
+        self.pins = motor.pins
+        self.pinsValues = (0, 0, 0)
+    def getStatus(self):
+        return (pins, pinsValues)
+
+    def setValues(self, newValues):
+        self.pinsValues = newValues
+
+    def run(self, cw, speed):
+        self.setValues(cw, not cw, speed)
+        
