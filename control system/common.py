2751ad5d3acfa406af83079d264e02440b964982
bshrram <bshoor96@gmail.com>
2020-06-22 05:54:07 +0300

    Create common.py

---
+++ control system/common.py
@@ -0,0 +1,14 @@
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
