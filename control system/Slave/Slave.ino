9de42b7fc62a2b5921fcc28eb3280eeb583c5b1d
bshrram <bshoor96@gmail.com>
2020-06-24 00:02:10 +0300

    Update Slave.ino

--- control system/Slave/Slave.ino
+++ control system/Slave/Slave.ino
@@ -6,50 +6,12 @@ Queue q(sizeof(int *), 100, IMPLEMENTATION);
 
 void setup()
 {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  //-----4th
-  pinMode(7, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(5, OUTPUT);
-  //-----5 th
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  //--- 6 th
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  //---------------------digital
-  //-----first
-  pinMode(48, OUTPUT);
-  pinMode(49, OUTPUT);
-  pinMode(50, OUTPUT);
-  pinMode(51, OUTPUT);
-  pinMode(52, OUTPUT);
-  pinMode(53, OUTPUT);
-  //-----secand
-  pinMode(34, OUTPUT);
-  pinMode(35, OUTPUT);
-  pinMode(36, OUTPUT);
-  pinMode(37, OUTPUT);
-  pinMode(38, OUTPUT);
-  pinMode(47, OUTPUT);
-  //-----3th
-  pinMode(28, OUTPUT);
-  pinMode(29, OUTPUT);
-  pinMode(30, OUTPUT);
-  pinMode(31, OUTPUT);
-  pinMode(32, OUTPUT);
-  pinMode(33, OUTPUT);
-  //-----4th
-  pinMode(22, OUTPUT);
-  pinMode(23, OUTPUT);
-  pinMode(24, OUTPUT);
-  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);
-  pinMode(27, OUTPUT);
+  for (int i = 2; i <= 53; i++)
+  {
+    if (i == 20 || i == 21)
+      continue;
+    pinMode(i, OUTPUT);
+  }
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onReceive(receiveEvent); // register event
   Serial.begin(9600);           // start serial for output
@@ -95,7 +57,7 @@ void handleMotor(int a[])
 
   else if (code == 3)
   {
-        analogWrite(a[1], a[2]);
+    analogWrite(a[1], a[2]);
   }
 }
 
