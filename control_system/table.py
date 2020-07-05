46a792bf0ed05c9f7d15edffac61c247df972345
Mohammad Ali Aboujaib <67196999+aliaboujaib@users.noreply.github.com>
2020-07-06 01:27:57 +0300

    update goToCell, getAngle. add goToLocation, move, rotate

--- control_system/table.py
+++ control_system/table.py
@@ -2,6 +2,8 @@ from cell import Cell
 from common import *
 import math
 
+row = 4
+col = 5
 class Table:
     """Table class that contains cells
     Attributes:
@@ -27,6 +29,9 @@ class Table:
     def getCellByLocation(self, location):
         """location: list
         """
+        [x, y] = location
+        if x>=row or x<0 or y<0 or y>=col:
+            return None
         myCell = next((i for i in self.cells if i.location == location), None)
         return myCell
 
@@ -46,30 +51,47 @@ class Table:
         result = []
         for i in range(howMany):
             result.append(self.cells[distance[i][1]])
-            print((distance[i][0], result[i].location, result[i].coordinates))
+            # print((distance[i][0], result[i].location, result[i].coordinates))
         return result
 
-    def getAngle(self, startCell, endCell):
-        (x1, y1) = startCell.coordinates
-        (x2, y2) = endCell.coordinates
+    def getAngle(self, startLocation, endLocation):
+        (x1, y1) = startLocation
+        (x2, y2) = endLocation
         x = x2 - x1
         y = y1 - y2
         a = math.atan2(y, x)
         a = math.degrees(a)
-        # if x<0 and y>=0:
-        #     a += 90
-        # elif x<0 and y<0:
-        #     a+=180
-        # elif x>=0 and y<0:
-        #     a = 360 - a
+        a -= 90
+        if a <0:
+            a = 360 + a
         return a
     
-    def goToCell(self, cell, cells):
+    def goToCell(self, cell, cells, boxCenter):
         runningCells = []
         for i in range(len(cells)):
             comCells = self.getCommonCells(cells[i])
-            angle  = self.getAngle(cells[i], cell)
-            cells[i].move(angle, 250, 0, comCells)
+            if (cell.id == cells[i].id):
+                angle = self.getAngle(boxCenter, cell.coordinates)
+                self.move(cell, angle, 200, 0)
+                runningCells += comCells
+                continue
+            angle  = self.getAngle(cells[i].coordinates, cell.coordinates)
+            self.move(cells[i], angle, 200, 0)
+            runningCells += comCells
+
+        restCells = [cell for cell in self.cells if cell not in runningCells]
+        for i in range(len(restCells)):
+            if restCells[i].getStatus() == (-1, -1, -1):
+                continue
+            comCells = self.getCommonCells(restCells[i])
+            restCells[i].stop(comCells)
+
+    def goToLocation(self, location, cells):
+        runningCells = []
+        for i in range(len(cells)):
+            comCells = self.getCommonCells(cells[i])
+            angle  = self.getAngle(cells[i].coordinates, location)
+            cells[i].move(angle, 200, 0, comCells)
             runningCells += comCells
 
         restCells = [cell for cell in self.cells if cell not in runningCells]
@@ -80,4 +102,9 @@ class Table:
             restCells[i].stop(comCells)
 
             
-        
+    def move(self, cell, angle, speed, w):
+        comCells = self.getCommonCells(cell)
+        cell.move(angle, speed, w, comCells)
+
+    def rotate(self, cell, angle):
+        self.move(cell, 0, 0, )
